fa6c84203d1ab5187321c45858011a6ffb43ce5b
Roman <cisinskijroman@gmail.com>
2025-01-27 12:23:28 +0200

    Add CAR

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 print('Roman nigga')
+
+def main(a, b):
+    return a**b
+
+res = main(2, 6)
+print(res)
+
+class Car():
+    x = 100
+    y = 400
+
+    def drive(self, x, y):
+        print(f'go to {x} and {y}')
+
+lamba = Car()
+
+print(lamba.y)
+lamba.drive(lamba.x, lamba.y)
+
